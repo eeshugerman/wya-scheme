084700d5f91959b637994d70dd166c3d599e0bdf
Elliott Shugerman <eeshugerman@gmail.com>
2021-01-12 00:18:54 -0500

    implement quasiquote properly (!?!??)

--- app/Evaluator.hs
+++ app/Evaluator.hs
@@ -3,18 +3,47 @@ import Control.Monad.Except ( throwError )
 import Types ( LispVal (..), LispError (..), LispValOrError )
 import Primatives ( primatives )
 
+sugarify :: String -> LispVal -> LispVal
+sugarify symbol val = LispList [LispSymbol symbol, val]
 
--- TODO: nesting doesn't work
+unquote :: LispVal -> LispVal
+unquote val = LispList [LispSymbol "unquote", val]
+
+quasiquote :: LispVal -> LispVal
+quasiquote val = LispList [LispSymbol "quasiquote", val]
+
 evalQuasiquoted :: [LispVal] -> LispValOrError
 evalQuasiquoted =
-  let iter :: [LispVal] -> [LispVal] -> LispValOrError
-      iter acc [] = return $ LispList acc
-      iter acc (LispList [LispSymbol "unquote", unquoted]:xs) =
-        case eval unquoted of
-          Left err -> throwError err
-          Right val -> iter (acc ++ [val]) xs
-      iter acc (x:xs) = iter (acc ++ [x]) xs
-  in iter []
+  let iter
+        :: Integer    -- quasiquote level
+        -> [LispVal]  -- accumulator
+        -> [LispVal]  -- remaining
+        -> LispValOrError
+      iter _ acc [] = return $ LispList $ reverse acc
+      iter qqDepth acc (LispList [LispSymbol "unquote", unquoted]:xs) =
+        if qqDepth == 1 then doEval else doSemiEval
+        where
+          doEval = case eval unquoted of
+            Left err -> throwError err
+            Right x -> iter qqDepth (x:acc) xs
+
+          doSemiEval = case unquoted of
+            LispList list ->
+              case iter (qqDepth - 1) [] list of
+                Left err -> throwError err
+                Right x -> iter qqDepth (unquote x:acc) xs
+            val -> return val
+
+      iter qqDepth acc (LispList [LispSymbol "quasiquote", quasiquoted]:xs) =
+        case quasiquoted of
+          LispList list ->
+            case iter (qqDepth + 1) [] list of
+              Left err -> throwError err
+              Right x -> iter qqDepth (quasiquote x:acc) xs
+          val -> return val
+
+      iter qqDepth acc (unevaled:xs) = iter qqDepth (unevaled:acc) xs
+  in iter 1 []
 
 eval :: LispVal -> LispValOrError
 eval val@(LispBool _)        = return val
@@ -33,7 +62,6 @@ eval (LispList (LispSymbol funcName : args)) = mapM eval args >>= func
     func args' = case lookup funcName primatives of
       Nothing     -> throwError $ NotAFunction "Unknown primitive function" funcName
       Just func'  -> func' args'
-
 
 eval form = throwError $ BadForm "Unrecognized form: " form
 
