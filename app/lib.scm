ab68b1d17212e2aacdbf1f54eea50993c4631bb1
Elliott Shugerman <eeshugerman@gmail.com>
2021-05-29 18:52:31 -0400

    add `else' and `=>' support for `cond' (needs testing)

--- app/lib.scm
+++ app/lib.scm
@@ -10,15 +10,19 @@
 
 (define (list . args) args)
 
+(define (length list)
+  (if (equal? list '())
+      0
+      (+ 1 (length (cdr list)))))
+
 (define (first l) (car l))
 (define (second l) (car (cdr l)))
+(define (third l) (car (cdr (cdr l))))
 
 (define (caar l) (car (car l)))
 (define (cdar l) (cdr (car l)))
 (define (cadr l) (car (cdr l)))
 (define (cddr l) (cdr (cdr l)))
-
-(define (cadar l) (car (cdr (car l))))
 
 (define (map proc l)
   (if (null? l)
@@ -30,14 +34,36 @@
   `(apply (lambda ,(map first bindings) (begin ,@expr))
           (list ,@(map second bindings))))
 
-(define-macro (cond . cond-clauses)
+(define-macro (cond . all-clauses)
   (define (loop clauses)
     (if (null? clauses)
-        '(#t ())
-        `(if ,(caar clauses)
-             ,(cadar clauses)
-             ,(loop (cdr clauses)))))
-  (loop cond-clauses))
+        ''()
+        (let ((clause (car clauses))
+              (clause-length (length (car clauses))))
+          (if (= clause-length 1)
+              `(or ,clause
+                   ,(loop (cdr clauses)))
+              (if (= clause-length 2)
+                  (if (equal? (car clause) 'else)
+                      (if (= (length clauses) 1)
+                          `(begin ,@(cdr clause))
+                          (raise (bad-form-error
+                                  "`else' in invalid position"
+                                  (cons 'cond all-clauses))))
+                      `(if ,(car clause)
+                           (begin ,@(cdr clause))
+                           ,(loop (cdr clauses))))
+                  (if (and (= clause-length 3)
+                           (equal? (second clause) '=>))
+                      (let ((test-result-var (gensym)))
+                        `(let ((,test-result-var ,(car clause)))
+                           (if ,test-result-var
+                               (,(third clause) ,test-result-var)
+                               ,(loop (cdr clauses)))))
+                      (raise (bad-form-error
+                              "invalid `cond' clause'"
+                              clause))))))))
+  (loop all-clauses))
 
 
 (define *gensym-counter* 0)
